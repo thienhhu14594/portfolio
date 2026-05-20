034ec13eb80fceaffb3b93282cba43130d32301a
thienhhu14594 <thienhhu@gmail.com>
2026-05-20 17:55:00 +0700

    Backend first check

---
+++ backend/src/main/java/com/thienhhu/backend/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.thienhhu.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+    @GetMapping("/health")
+    public String health() {
+        return "Backend running";
+    }
+}
